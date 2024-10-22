026b4cc8bcafcb310ccfbcdb0619c1b7236565f3
techpilot <ngwustephen99@gmail.com>
2024-10-22 17:37:52 +0100

    implemeted about page

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css',
+})
+export class AboutComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { NewLandingPageComponent } from './new-landing-page/new-landing-page.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', component: NewLandingPageComponent },
+  { path: 'about', component: AboutComponent },
 ];
