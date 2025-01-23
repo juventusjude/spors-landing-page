e19a8ac0a774e3d1cb847bf63d6cc55cfaea82d0
techpilot <ngwustephen99@gmail.com>
2025-01-23 14:54:41 +0100

    added social icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,5 @@ import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', component: NewLandingPageComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'learn-more', component: AboutComponent },
 ];
